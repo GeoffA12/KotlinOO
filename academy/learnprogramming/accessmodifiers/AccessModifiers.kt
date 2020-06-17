230b1fe19453870547da027e84eb0d403db3735d
arrgeoff <arrgeoff@amazon.com>
2020-06-17 17:37:50 -0500

    Inheritance, functions, and interface code all added.

--- academy/learnprogramming/accessmodifiers/AccessModifiers.kt
+++ academy/learnprogramming/accessmodifiers/AccessModifiers.kt
@@ -7,9 +7,10 @@ fun main(args: Array<String>) {
     val p = Person("John")
     println(p.firstName)
     val b = Boss("John")
-    println(b)
-    val b2 = Boss("John", false)
-    println(b2)
+    println(b.firstName)
+    val b2 = Boss("John")
+    println(b2.firstName)
+    println(b2.fullTime)
 }
 
 // This would be impossible in Java. But because of private access modifiers in Kotlin, where other code in this
@@ -28,13 +29,14 @@ class Employee constructor(firstName: String){
     }
 }
 
-// MORE VERBOSE WAY OF WRITING A CLASS WITH A CONSTRUCTOR (EQUIVALENT TO FIRST EXAMPLE)
+// LESS VERBOSE WAY OF WRITING A CLASS WITH A CONSTRUCTOR (EQUIVALENT TO FIRST EXAMPLE)
 class Person constructor(firstName: String) {
     val firstName: String = firstName
 }
 
-// EVEN MORE VERBOSE WAY (EQUIVALENT TO FIRST AND SECOND EXAMPLE)
-class Boss(val firstName: String) {
+// EVEN LESS VERBOSE WAY (EQUIVALENT TO FIRST AND SECOND EXAMPLE)
+// fullTime parameter has a default value of true if not passed into the constructor.
+class Boss(val firstName: String, var fullTime: Boolean = true) {
     // We now have access to a class property firstName which is of type string and deonted by 'firstName'
 
     // add a secondary constructor. Notice that when we have a secondary constructor in kotlin, we must delegate
@@ -42,9 +44,9 @@ class Boss(val firstName: String) {
     // The syntax here is basically binding this secondary constructor onto the primary constructor with
     // the this(firstName) code.
     // Also, we can't use val and var in the secondary constructor because secondary constructors can't declare
-    // properties for us in the constructor definition like they can in the primary constructor.
-    var fullTime: Boolean = true
-    constructor(firstName: String, fullTime: Boolean): this(firstName) {
-        this.fullTime = fullTime
-    }
+    // properties for us in the constructor definition like they can in the primary constructor
+    // var fullTime = true
+//    constructor(firstName: String, fullTime: Boolean): this(firstName) {
+//        this.fullTime = fullTime
+//    }
 }
